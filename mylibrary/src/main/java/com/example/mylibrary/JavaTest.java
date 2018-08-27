3b1d4f69e5dd2af1c26898a6b760fa0e87a3ccd1
912696973 <912696973@qq.com>
2018-08-27 17:52:13 +0800

    测试

--- mylibrary/src/main/java/com/example/mylibrary/JavaTest.java
+++ mylibrary/src/main/java/com/example/mylibrary/JavaTest.java
@@ -1,9 +1,15 @@
 package com.example.mylibrary;
 
+import android.text.TextUtils;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.reactivex.Observable;
+import io.reactivex.Single;
 
 /**
  * @author: xuzhiyong
@@ -20,11 +26,29 @@ public class JavaTest {
 
 //        byte[] bytes = new byte[1];
 //        new String(bytes);
+//
+//        String date = "2018-08-06 02:44:15";
+//        String date1 = "2018-08-06 02:44:15";
+//        System.out.println(isSameDay(date,date1));
 
-        String date = "2018-08-06 02:44:15";
-        String date1 = "2018-08-06 02:44:15";
-        System.out.println(isSameDay(date,date1));
+        AtomicInteger count = new AtomicInteger();
 
+//        Observable.range(1, 10)
+//                .doOnNext(ignored -> count.incrementAndGet())
+//                .ignoreElements()
+//                .andThen(Single.just(count.get()))
+//                .subscribe(System.out::println);
+
+
+        Observable.range(1, 10)
+                .doOnNext(ignored ->
+                {
+                    count.incrementAndGet();
+                    System.out.println("1");
+                })
+                .ignoreElements()
+                .andThen(Single.defer(() -> Single.just(count.get())))
+                .subscribe(System.out::println);
 
 //        final ArrayList<Person> personArrayList = new ArrayList<>();
 //
@@ -47,6 +71,13 @@ public class JavaTest {
 //            e.printStackTrace();
 //        }
 //        System.out.println(Arrays.toString(personArrayList.toArray()));
+        String name = getName();
+        System.out.println(name == null);
+
+    }
+
+    public static String getName(){
+        return null;
     }
 
 
