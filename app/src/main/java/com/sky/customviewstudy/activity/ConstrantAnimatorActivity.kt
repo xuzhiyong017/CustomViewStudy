0d77033eb7022ee2afac039bbc8d54bab024b813
xuzhiyong <12345678>
2020-08-10 13:05:34 +0800

    约束布局动画实现ConstraintSet使用

--- app/src/main/java/com/sky/customviewstudy/activity/ConstrantAnimatorActivity.kt
+++ app/src/main/java/com/sky/customviewstudy/activity/ConstrantAnimatorActivity.kt
@@ -3,7 +3,10 @@ package com.sky.customviewstudy.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
+import android.transition.TransitionManager
+import android.view.View
 import com.sky.customviewstudy.R
+import kotlinx.android.synthetic.main.activity_constrant_animator.*
 
 class ConstrantAnimatorActivity : AppCompatActivity() {
 
@@ -13,5 +16,21 @@ class ConstrantAnimatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constrant_animator)
+
+        constraintSetEnd.clone(this,R.layout.activity_constrant_animator_action)
+        constraintSetStart.clone(root_view)
+    }
+
+    var boolean = true
+
+    fun changeAnimator(v:View?){
+        TransitionManager.beginDelayedTransition(root_view)
+        if(boolean){
+            boolean = false
+            constraintSetEnd.applyTo(root_view)
+        }else{
+            boolean = true
+            constraintSetStart.applyTo(root_view)
+        }
     }
 }
